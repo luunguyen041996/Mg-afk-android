573577da40350cfadbf0487b3fca497c7ef183de
luunguyen041996 <luunguyen041996@gmail.com>
2026-04-24 12:52:26 +0700

    fix: remove unused EmbeddedServer import

--- app/src/main/java/com/mgafk/app/service/RemoteControlServer.kt
+++ app/src/main/java/com/mgafk/app/service/RemoteControlServer.kt
@@ -8,7 +8,6 @@ import com.mgafk.app.ui.UiState
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
-import io.ktor.server.engine.EmbeddedServer
 import io.ktor.server.netty.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
